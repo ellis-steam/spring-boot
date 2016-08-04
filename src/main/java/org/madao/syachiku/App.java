179f945f100ee3addd028b2f5fd83dc860f31aee
Ellis <ellis@s-team.com.tw>
2016-08-04 14:31:07 +0800

    Setting up Apache Tiles - added apache tiles-core and tiles-jsp, javax.servlet-jstl dependencies. - added 2 methods tilesConfigurer and tilesViewResolver for tiles first setup **tiles is not good to go yet at this stage**

--- src/main/java/org/madao/syachiku/App.java
+++ src/main/java/org/madao/syachiku/App.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer{
@@ -21,6 +25,26 @@ public class App extends SpringBootServletInitializer{
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(App.class);
+	}
+	
+	@Bean
+	public TilesConfigurer tilesConfigurer(){
+		TilesConfigurer tilesConfigurer = new TilesConfigurer();
+		
+		String[] defs = {"/WEB-INF/tiles.xml"};
+		
+		tilesConfigurer.setDefinitions(defs);
+		
+		return tilesConfigurer;
+	}
+	
+	@Bean
+	public UrlBasedViewResolver tilesViewResolver() {
+		UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver();
+		
+		tilesViewResolver.setViewClass(TilesView.class);
+		
+		return tilesViewResolver;
 	}
 	
 }
